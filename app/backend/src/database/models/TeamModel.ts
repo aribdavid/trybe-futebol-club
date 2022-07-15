bfc6557785c12b9e69771bb77ba6daae891acdf4
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-07-15 16:26:12 -0300

    feat: created team model

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id!: number;
+  public teamName!: string;
+  teamHome: {
+    id: number;
+    homeTeam: number;
+    homeTeamGoals: number;
+    awayTeam: number;
+    awayTeamGoals: number;
+    inProgress: boolean;
+  }[];
+
+  teamAway: {
+    id: number;
+    homeTeam: number;
+    homeTeamGoals: number;
+    awayTeam: number;
+    awayTeamGoals: number;
+    inProgress: boolean;
+  }[];
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
