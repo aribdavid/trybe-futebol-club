06a606e0787f492812a7b938cdc3fe86fae418f6
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-07-15 16:36:38 -0300

    feat: created Team controller

---
+++ app/backend/src/controllers/TeamController.ts
@@ -0,0 +1,20 @@
+import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express';
+import { ITeamService } from '../interfaces/ITeam';
+
+export default class TeamController {
+  constructor(private _service: ITeamService) {}
+
+  async getAll(_req: Request, res: Response) {
+    const teams = await this._service.getAll();
+
+    res.status(StatusCodes.OK).json(teams);
+  }
+
+  async getById(req: Request, res: Response) {
+    const { id } = req.params;
+    const team = await this._service.getById(Number(id));
+
+    res.status(StatusCodes.OK).json(team);
+  }
+}
