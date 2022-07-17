f39123b0dd0615def78aaa01dadd73b4804cd28d
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-07-17 10:21:44 -0300

    feat: created IMatch interface

---
+++ app/backend/src/interfaces/IMatch.ts
@@ -0,0 +1,35 @@
+import Match from '../database/models/MatchModel';
+
+export interface IMatch extends Match {
+  teamHome: {
+    teamName: string
+  },
+  teamAway: {
+    teamName: string
+  }
+}
+
+export type TUpdateGoals = Omit<Match, 'id' | 'inProgress' | 'homeTeam' | 'awayTeam'>;
+
+export interface IMatchWithTeam {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+export interface IMatchModel {
+  getAll(inProgress: boolean | undefined): Promise<IMatch[]>
+  create(data: Omit<Match, 'id' | 'inProgress'>): Promise<Match>
+  updateFinished(id: number): Promise<void>
+  updateGoals(id:number, data: TUpdateGoals): Promise<void>
+}
+
+export interface IMatchService {
+  getAll(query: boolean | undefined): Promise<IMatch[]>
+  create(data: Omit<Match, 'id' | 'inProgress'>): Promise<Match>
+  updateFinished(id: number): Promise<void>
+  updateGoals(id:number, data: TUpdateGoals): Promise<void>
+}
