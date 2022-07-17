2472c9db6da306a7a4604afc92d3d4d8309b388a
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-07-17 10:42:45 -0300

    feat: created leaderboard interface

---
+++ app/backend/src/interfaces/ILeaderboard.ts
@@ -0,0 +1,24 @@
+export interface ILeaderboard {
+  name: string
+  totalPoints: number
+  totalGames: number
+  totalVictories: number
+  totalDraws: number
+  totalLosses: number
+  goalsFavor: number
+  goalsOwn: number
+  goalsBalance: number
+  efficiency: number
+}
+
+export interface ILeaderboardModel {
+  getHomeTeam(): Promise<ILeaderboard[]>
+  getAwayTeam(): Promise<ILeaderboard[]>
+  getAll(): Promise<ILeaderboard[]>
+}
+
+export interface ILeaderboardService {
+  getHomeTeam(): Promise<ILeaderboard[]>
+  getAwayTeam(): Promise<ILeaderboard[]>
+  getAll(): Promise<ILeaderboard[]>
+}
