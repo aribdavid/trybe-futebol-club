8afa3d5b0e6734ec495c1f8044e939a3aed93cbd
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-07-17 10:53:40 -0300

    feat: created leaderboard controller

---
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { ILeaderboardService } from '../interfaces/ILeaderboard';
+
+export default class LeaderboardController {
+  constructor(private _service: ILeaderboardService) {}
+
+  async getHomeLeaderboard(_req: Request, res: Response) {
+    const leaderboard = await this._service.getHomeTeam();
+    return res.status(200).json(leaderboard);
+  }
+
+  async getAwayLeaderboard(_req: Request, res: Response) {
+    const leaderboard = await this._service.getAwayTeam();
+    return res.status(200).json(leaderboard);
+  }
+
+  async getAll(_req: Request, res: Response) {
+    const leaderboard = await this._service.getAll();
+    return res.status(200).json(leaderboard);
+  }
+}
